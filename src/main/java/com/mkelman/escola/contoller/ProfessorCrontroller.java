6c29733dbfbd6570bd5ca84ae75af56187d1dd10
Mkelman18 <manuela.denega@escola.pr.gov.br>
2025-07-08 09:34:35 -0300

    metodo delete

--- src/main/java/com/mkelman/escola/contoller/ProfessorCrontroller.java
+++ src/main/java/com/mkelman/escola/contoller/ProfessorCrontroller.java
@@ -5,7 +5,9 @@ import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +42,12 @@ public class ProfessorCrontroller {
         return professorRespository.findAll();
     }
 
-
+    @DeleteMapping (value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        professorRespository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    
+     }
 
     @PostMapping( value ="/insert") 
     public ResponseEntity<?> insert(@RequestBody ProfessorDto professorDto) {
